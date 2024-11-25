6b077213f6407fb3aa19647dfc736e7badf59b85
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-25 15:20:15 -0300

    agrego estilos

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+ container: {
+  padding: 20,
+  paddingBottom: 5,
+  paddingTop: 5
+ },
+ strong: {
+  color: '#09f',
+  fontWeight: 'bold',
+  marginBottom: 5
+ }
+})
 
 const RepositoryItem = (props) => (
-  <View key={props.id} style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}>
- <Text style={{ fontWeight: "bold", marginBottom: 5 }}>id: {props.fullName}</Text>
- <Text>FullName: {props.id}</Text>
- <Text>Description: {props.description}</Text>
- <Text>Language: {props.language}</Text>
- <Text>Stars: {props.stargazersCount}</Text>
- <Text>Forks: {props.forksCount}</Text>
- <Text>Review: {props.reviewCount}</Text>
- <Text>Rating: {props.ratingAverage}</Text>
+<View key={props.id} style={styles.container}>
+  <Text style={styles.strong}>id: {props.id}</Text>
+  <Text>FullName: {props.fullName}</Text>
+  <Text>Description: {props.description}</Text>
+  <Text>Language: {props.language}</Text>
+  <Text>Stars: {props.stargazersCount}</Text>
+  <Text>Forks: {props.forksCount}</Text>
+  <Text>Review: {props.reviewCount}</Text>
+  <Text>Rating: {props.ratingAverage}</Text>
 </View>
 )
 
