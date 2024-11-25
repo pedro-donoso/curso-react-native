35777580a3f62585095b0790b4ea41aa6e80d2cc
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-25 15:21:29 -0300

    funciona

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native'
 
-const styles = StyleSheet.create({
- container: {
-  padding: 20,
-  paddingBottom: 5,
-  paddingTop: 5
- },
- strong: {
-  color: '#09f',
-  fontWeight: 'bold',
-  marginBottom: 5
- }
-})
-
 const RepositoryItem = (props) => (
 <View key={props.id} style={styles.container}>
   <Text style={styles.strong}>id: {props.id}</Text>
@@ -26,5 +13,18 @@ const RepositoryItem = (props) => (
   <Text>Rating: {props.ratingAverage}</Text>
 </View>
 )
+
+const styles = StyleSheet.create({
+ container: {
+  padding: 20,
+  paddingBottom: 5,
+  paddingTop: 5,
+ },
+ strong: {
+  color: "#09f",
+  fontWeight: "bold",
+  marginBottom: 5,
+ },
+});
 
 export default RepositoryItem
