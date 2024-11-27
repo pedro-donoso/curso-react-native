9a525f9ea1b23fa1e6ecffecca42f72dbfa5f81d
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-27 15:38:57 -0300

    agrego flex

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,15 +2,35 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native'
 import StyledText from './StyledText';
 
+const RepositoryStats = props => {
+  return (
+   <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+    <View>
+     <StyledText fontWeight='bold'>Stars</StyledText>
+     <StyledText>{props.stargazersCount}</StyledText>
+    </View>
+    <View>
+     <StyledText fontWeight='bold'>Forks</StyledText>
+     <StyledText>{props.forksCount}</StyledText>
+    </View>
+    <View>
+     <StyledText fontWeight='bold'>Review</StyledText>
+     <StyledText>{props.reviewCount}</StyledText>
+    </View>
+    <View>
+     <StyledText fontWeight='bold'>Rating</StyledText>
+     <StyledText>{props.ratingAverage}</StyledText>
+    </View>
+   </View>
+  );
+}
+
 const RepositoryItem = (props) => (
 <View key={props.id} style={styles.container}>
   <StyledText fontSize='subheading' fontWeight='bold'>{props.fullName}</StyledText>
   <StyledText bold>{props.description}</StyledText>
   <StyledText blue>{props.language}</StyledText>
-  <StyledText small>Stars: {props.stargazersCount}</StyledText>
-  <StyledText small>Forks: {props.forksCount}</StyledText>
-  <StyledText small>Review: {props.reviewCount}</StyledText>
-  <StyledText small>Rating: {props.ratingAverage}</StyledText>
+  <RepositoryStats {...props} />
 </View>
 )
 
