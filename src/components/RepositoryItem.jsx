a783051639d2ce306ba08661d428a02d97397595
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-27 16:41:58 -0300

    paso props

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native'
-import StyledText from './StyledText';
-import RepositoryStats from './RepositoryStats';
-import theme from '../theme';
+import React from "react";
+import { Image, View, StyleSheet } from "react-native";
+import StyledText from "./StyledText";
+import RepositoryStats from "./RepositoryStats";
+import theme from "../theme";
 
-const RepositoryItem = (props) => (
-<View key={props.id} style={styles.container}>
-  <StyledText fontSize='subheading' fontWeight='bold'>{props.fullName}</StyledText>
+const RepositoryItemHeader = (props) => (
+ <View>
+  <Image style={styles.image} source={{ uri: props.ownerAvatarUrl }} />
+  <StyledText fontSize="subheading" fontWeight="bold">
+   {props.fullName}
+  </StyledText>
   <StyledText>{props.description}</StyledText>
   <StyledText style={styles.language}>{props.language}</StyledText>
+ </View>
+);
+
+const RepositoryItem = (props) => (
+ <View key={props.id} style={styles.container}>
+  <RepositoryItemHeader {...props} />
   <RepositoryStats {...props} />
-</View>
-)
+ </View>
+);
 
 const styles = StyleSheet.create({
  container: {
@@ -23,8 +32,15 @@ const styles = StyleSheet.create({
   padding: 4,
   color: theme.colors.white,
   backgroundColor: theme.colors.primary,
-  alignSelf: 'flex-start'
- }
+  alignSelf: "flex-start",
+  borderRadius: 4,
+  overflow: "hidden",
+ },
+ image: {
+  width: 64,
+  height: 64,
+  borderRadius: 4,
+ },
 });
 
-export default RepositoryItem
+export default RepositoryItem;
