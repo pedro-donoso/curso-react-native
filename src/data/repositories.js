51712b435c0a4e3477d0e77998997607431b37b9
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-27 16:30:53 -0300

    agrego cuenta github

--- src/data/repositories.js
+++ src/data/repositories.js
@@ -1,24 +1,26 @@
 export default [
  {
-  id: "midudev.midu.dev",
-  fullName: "midudev/midu.dev",
+  id: "https://github.com/pedro-donoso",
+  fullName: "github.com/pedro-donoso",
   description: "Blog de noticias",
   language: "HTML",
   forksCount: 3123,
   stargazersCount: 52869,
   ratingAverage: 0,
   reviewCount: 0,
-  ownerAvatarUrl: "https://avatars.githubusercontent.com/u/1561955?v=4",
+  ownerAvatarUrl:
+   "https://avatars.githubusercontent.com/u/68760595?s=400&u=412990d1bf145e06f74866dbb0dfd99fa0d28219&v=4",
  },
  {
-  id: "midudev2.midu.dev",
-  fullName: "midudev2/midu2.dev",
+  id: "https://github.com/pedro-donoso",
+  fullName: "github.com/pedro-donoso",
   description: "Noticias sobre desarrollo",
   language: "JavaScript",
   forksCount: 4401,
   stargazersCount: 10968,
   ratingAverage: 11,
   reviewCount: 95,
-  ownerAvatarUrl: "https://avatars.githubusercontent.com/u/1561955?v=4",
- }
+  ownerAvatarUrl:
+   "https://avatars.githubusercontent.com/u/68760595?s=400&u=412990d1bf145e06f74866dbb0dfd99fa0d28219&v=4",
+ },
 ];
