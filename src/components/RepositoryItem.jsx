70eb5993505634663cd57a12685865b995c1403f
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-27 15:58:23 -0300

    modifico estilos

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,29 +1,7 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native'
 import StyledText from './StyledText';
-
-const RepositoryStats = props => {
-  return (
-   <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
-    <View>
-     <StyledText fontWeight='bold'>Stars</StyledText>
-     <StyledText>{props.stargazersCount}</StyledText>
-    </View>
-    <View>
-     <StyledText fontWeight='bold'>Forks</StyledText>
-     <StyledText>{props.forksCount}</StyledText>
-    </View>
-    <View>
-     <StyledText fontWeight='bold'>Review</StyledText>
-     <StyledText>{props.reviewCount}</StyledText>
-    </View>
-    <View>
-     <StyledText fontWeight='bold'>Rating</StyledText>
-     <StyledText>{props.ratingAverage}</StyledText>
-    </View>
-   </View>
-  );
-}
+import RepositoryStats from './RepositoryStats';
 
 const RepositoryItem = (props) => (
 <View key={props.id} style={styles.container}>
--- src/components/StyledText.jsx
+++ src/components/StyledText.jsx
@@ -20,12 +20,16 @@ const styles = StyleSheet.create({
   },
   subheading: {
     fontSize: theme.fontSizes.subheading
+  },
+  textAlignCenter: {
+    textAlign: 'center'
   }
 })
 
-export default function StyledText ({children, color, fontSize, fontWeight, style, ...restOfProps}) {
+export default function StyledText ({align, children, color, fontSize, fontWeight, style, ...restOfProps}) {
   const textStyles = [
     styles.text,
+    align === 'center' && styles.textAlignCenter,
     color === 'primary' && styles.colorPrimary,
     color === 'secondary' && styles.colorSecondary,
     fontSize === 'subheading' && styles.subheading,
